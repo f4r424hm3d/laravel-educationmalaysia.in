2a213d77768a0c9aebc1bc036ddc08d9cb4d0acc
MohdFaraz <farazahmad280@gmail.com>
2025-01-06 11:38:39 +0530

    Design change by gairav

--- resources/views/front/courses-in-malaysia.blade.php
+++ resources/views/front/courses-in-malaysia.blade.php
@@ -302,10 +302,10 @@
             @include('front.filter-courses-in-malaysia')
           </div>
       </div>
-      <div class="modal-footer">
+      <!-- <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+      </div> -->
     </div>
   </div>
 </div>
