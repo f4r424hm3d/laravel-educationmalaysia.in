ed4727b2c4d59e7747397b65c199eca5b8762a98
MohdFaraz <farazahmad280@gmail.com>
2025-02-03 09:13:02 +0530

    courses page breadcrumb error fix

--- resources/views/front/courses.blade.php
+++ resources/views/front/courses.blade.php
@@ -14,7 +14,9 @@
             <ul class="cources_facts_list">
               <li class="facts-1"><a href="{{ url('/') }}">Home</a></li>
               <li class="facts-1"><a href="{{ url('select-level') }}">Courses</a></li>
-              <li class="facts-1">{{ strtoupper($pageContent->page_name) }}</li>
+              @if ($pageContent)
+                <li class="facts-1">{{ strtoupper($pageContent->page_name) }}</li>
+              @endif
             </ul>
           </div>
         </div>
@@ -22,10 +24,9 @@
     </div>
   </div>
   <!-- Breadcrumb -->
-
-  <!-- === Hero Banner End ===-->
-  <section class="bg-light new-home-box">
-    @if ($pageContent != null)
+  @if ($pageContent)
+    <!-- === Hero Banner End ===-->
+    <section class="bg-light new-home-box">
       <div class="container">
         <div class="graduate_bx">
           <div class="show-more-box-country">
@@ -53,8 +54,8 @@
           </div>
         </div>
       </div>
-    @endif
-  </section>
+    </section>
+  @endif
 
   <section class="pop-cour">
     <div class="container com-sp">
