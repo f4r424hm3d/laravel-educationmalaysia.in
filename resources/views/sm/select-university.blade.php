3b06f2d0182654f79f732ed65b37ecb9d05770cf
MohdFaraz <farazahmad280@gmail.com>
2025-03-03 15:21:01 +0530

    Universities in malaysia sitemap route change

--- resources/views/sm/select-university.blade.php
+++ resources/views/sm/select-university.blade.php
@@ -3,25 +3,25 @@
 @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>{{ url('universities-in-malaysia') }}</loc>
+    <loc>{{ url('universities/universities-in-malaysia') }}</loc>
     <lastmod>{{ date('Y-m-d') }}</lastmod>
     <changefreq>always</changefreq>
     <priority>1</priority>
   </url>
   <url>
-    <loc>{{ url('public-universities-in-malaysia') }}</loc>
+    <loc>{{ url('universities/public-universities-in-malaysia') }}</loc>
     <lastmod>{{ date('Y-m-d') }}</lastmod>
     <changefreq>always</changefreq>
     <priority>1</priority>
   </url>
   <url>
-    <loc>{{ url('private-universities-in-malaysia') }}</loc>
+    <loc>{{ url('universities/private-universities-in-malaysia') }}</loc>
     <lastmod>{{ date('Y-m-d') }}</lastmod>
     <changefreq>always</changefreq>
     <priority>1</priority>
   </url>
   <url>
-    <loc>{{ url('foreign-universities-in-malaysia') }}</loc>
+    <loc>{{ url('universities/foreign-universities-in-malaysia') }}</loc>
     <lastmod>{{ date('Y-m-d') }}</lastmod>
     <changefreq>always</changefreq>
     <priority>1</priority>
