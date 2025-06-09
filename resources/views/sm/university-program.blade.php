86d5eea276b5a5d2f022f51e3acfadba7932c05c
MohdFaraz <farazahmad280@gmail.com>
2025-06-09 13:53:18 +0530

    sitemap route change

--- resources/views/sm/university-program.blade.php
+++ resources/views/sm/university-program.blade.php
@@ -4,7 +4,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   @foreach ($rows as $row)
     <url>
-      <loc>{{ url('university/' . $row->university->uname . '/course/' . $row->slug) }}</loc>
+      <loc>{{ url('university/' . $row->university->uname . '/courses/' . $row->slug) }}</loc>
       <lastmod>{{ $row->updated_at->format('Y-m-d') }}</lastmod>
       <changefreq>always</changefreq>
       <priority>0.5</priority>
