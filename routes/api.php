ea81fe83a498b980d2d57b808b89856f3fc9a9d5
MohdFaraz <farazahmad280@gmail.com>
2025-08-23 16:13:22 +0530

    Add in API

--- routes/api.php
+++ routes/api.php
@@ -112,4 +112,3 @@ Route::prefix('student')->group(function () {
     Route::post('/logout', [StudentProfileApi::class, 'logout']);
   });
 });
-Route::get('university-details/{uname}', [UniversityApi::class, 'universityDetail']);
